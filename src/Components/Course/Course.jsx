7e549f9788a9db729890921927fe6c907ef4afe9
Bestimran <imrantahir9918@gmail.com>
2024-03-27 23:38:41 +0600

    courses section added

---
+++ src/Components/Course/Course.jsx
@@ -0,0 +1,25 @@
+import PropsTypes from 'prop-types'
+const Course = ({course}) => {
+    const {cover, title, credit_hours, price, short_description}= course;
+    return (
+        <div className='space-y-4'>
+            <img className='w-full' src={cover} alt="" />
+            <h2 className='text-2xl font-semibold'>{title}</h2>
+            <p className='text-xl'>{short_description}</p>
+            <div className='flex justify-between'>
+                <div>
+                     <p>$ price:{price}</p> 
+                </div>
+                <div>
+                    <h3>credit: {credit_hours}</h3>
+                </div>
+            </div>
+            <button className='btn bg-green-500 w-full p-2 rounded text-xl font-bold'>select</button>
+        </div>
+    );
+};
+Course.propTypes= {
+    course: PropsTypes.object
+}
+
+export default Course;
--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -1,7 +1,24 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Course from "../Course/Course";
+
 const Courses = () => {
+    const [courses, setCourses] = useState([]);
+    console.log(courses)
+
+    useEffect(()=>{
+        fetch('../../../public/data.json')
+        .then(res => res.json())
+        .then(data => setCourses(data))
+    },[])
     return (
-        <div className="w-3/4">
-            <h1>Courses</h1>
+        <div className="w-3/4 grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-4"> 
+            {
+                courses.map(course=><Course 
+                key={course.id} 
+                course={course}
+                ></Course> )
+            }
         </div>
     );
 };
